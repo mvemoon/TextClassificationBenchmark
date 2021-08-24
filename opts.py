3c9349bce5923fc4efd9208cc20d9695ac7f3b67
mvemoon <85032713+mvemoon@users.noreply.github.com>
2021-08-24 11:12:25 +0800

    max_len 512

--- opts.py
+++ opts.py
@@ -13,7 +13,7 @@ class Params(object):
         parser.add_argument('--hidden_dim', type=int, default=128,
                         help='hidden_dim')     
     
-        parser.add_argument('--max_seq_len', type=int, default=200,
+        parser.add_argument('--max_seq_len', type=int, default=512,
                         help='max_seq_len')
         parser.add_argument('--batch_size', type=int, default=64,
                         help='batch_size')
